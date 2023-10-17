0d81c396aff234252b15867c1e3c68d189bc0afe
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2023-10-17 10:59:16 -0500

    solution

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
 $(document).ready(function() {
-    // Load tasks from localStorage and populate the list
+    $("form").submit(function(e) {
+        e.preventDefault(); // Prevent the form
+        var taskInput = $("input[name='task']");
+        var task = taskInput.val();
+
+
+            // Add the task to the list
+            $("#to-do-list ul").append("<li>" + task + "</li>");
+            taskInput.val("");
+
+            // Save the updated task list in localStorage
+            savedTasks.push(task);
+            localStorage.setItem("tasks", JSON.stringify(savedTasks));
+
+    });
+
+    // Load tasks from localStorage and populate list
     function loadTasks() {
         var savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
         savedTasks.forEach(function(task) {
             $("#to-do-list ul").append("<li>" + task + "</li>");
         });
     }
+
     loadTasks();
 
-    $("#task-form").submit(function(e) {
-        e.preventDefault(); // Prevent the form from submitting and refreshing the page
-        var taskInput = $("input[name='task']");
-        var task = taskInput.val();
-
-        if (task.trim() !== "") {
-            // Add the task to the list
-            $("#to-do-list ul").append("<li>" + task + "</li>");
-            taskInput.val("");
-
-            // Save the updated task list in localStorage
-            var savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
-            savedTasks.push(task);
-            localStorage.setItem("tasks", JSON.stringify(savedTasks));
-        }
-    });
-
+    ////handle delete function
     $("#to-do-list ul").on("click", "li", function() {
         $(this).toggleClass("crossOff");
         var task = $(this).text();
-        var savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+        var savedTasks = JSON.parse(localStorage.getItem("tasks"))
         var taskIndex = savedTasks.indexOf(task);
 
-        if (taskIndex !== -1) {
-            savedTasks.splice(taskIndex, 1);
-            localStorage.setItem("tasks", JSON.stringify(savedTasks));
-        }
+
+        savedTasks.splice(taskIndex, 1);
+        localStorage.setItem("tasks", JSON.stringify(savedTasks));
 
         $(this).remove();
+
     });
 });
