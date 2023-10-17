5940e0c776ac1f2e0f923baccb0eee9f73dd6487
Ayan Ahmed <ayanahmed@Ayans-MacBook-Air.local>
2023-10-16 21:00:24 -0500

    form submission handled, added cross off toggle

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function printData( ){
-    e.preventDefault();
+$(document).ready(function() {
 
-    var todo = $("form").serializeArray();
-    console.log(todo)
-    var task = todo[0].value;
-    $("#to-do-list").append("" + task +"</li>")
+    $("form").submit(function(e) {
+        e.preventDefault(); // Prevent the form from submitting and refreshing the page
+        var todo = $(this).serializeArray();
+        var task = todo[0].value;
 
-}
+         $("#to-do-list ul").append("<li>" + task + "</li>");
+         $("input[name='task']").val("");
+    });
+//
+    $("#to-do-list").on("click",  function(e) {
+        $(e.target).toggleClass("crossOff");
+    });
+});
+
+//step 1
+/// create a click event that strikes through a list item when you’ve completed it. 
+//That item should also be removed from the task list.
